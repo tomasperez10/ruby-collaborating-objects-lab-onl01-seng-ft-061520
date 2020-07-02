cc253413c0ad27feb0cbf5600ab5be4594c429da
Tomas Perez <tomasitopg6@gmail.com>
2020-07-02 18:08:56 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,51 @@
+class Artist
+  
+  attr_accessor :name, :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+  def self.find(name)
+    @@all.find do |artist|
+      artist.name == name
+    end
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist
+  end
+
+  def print_songs
+    songs.collect do |song|
+      puts song.name
+    end
+  end
+  
+end
